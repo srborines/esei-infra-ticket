86a67418b31b6d6785f8265ab336df6e1d0ceac1
Baltasar <baltasarq@gmail.com>
2018-03-21 13:06:32 +0100

    Added search

--- handlers/manage_tickets.py
+++ handlers/manage_tickets.py
@@ -1,9 +1,10 @@
 # infra-esei-tickets (c) Baltasar 2018 MIT License <baltasarq@gmail.com>
 
 import webapp2
+import logging
 from webapp2_extras import jinja2
 from google.appengine.api import users
-
+from google.appengine.ext import ndb
 
 from model.appinfo import AppInfo
 from model.ticket import Ticket
@@ -11,13 +12,41 @@ import model.user as usr_mgt
 
 
 class TicketsManager(webapp2.RequestHandler):
+    @staticmethod
+    def filter(tickets, list_search_terms):
+        result_set_keys = []
+        list_search_terms = [x.lower() for x in list_search_terms]
+
+        if list_search_terms:
+            for ticket in ndb.get_multi(tickets):
+                found = False
+                title = str(ticket.title).lower()
+                desc = str(ticket.desc).lower()
+
+                for search_term in list_search_terms:
+                    logging.info("Looking for " + search_term + " in '" + title + "' and: " + desc)
+                    if (search_term in title
+                       or search_term in desc):
+                        found = True
+                        break
+
+                if found:
+                    result_set_keys.append(ticket.key)
+        else:
+            result_set_keys = tickets
+
+        return ndb.get_multi(result_set_keys)
+
     def get(self):
         try:
             show_all = self.request.GET['show_all']
+            search_terms = self.request.GET['search']
         except:
             show_all = "false"
+            search_terms = ""
 
         show_all = True if show_all == "true" else False
+        list_search_terms = search_terms.split()
 
         user = users.get_current_user()
         
@@ -28,13 +57,14 @@ class TicketsManager(webapp2.RequestHandler):
 
             # Retrieve the relevant tickets
             tickets = Ticket.query().order(-Ticket.added)
-
             if not show_all:
                 tickets = tickets.filter(Ticket.status == Ticket.Status.Open)
 
             if usr_info.is_client():
                 tickets = tickets.filter(Ticket.owner_email == usr_info.email
                                          or Ticket.client_email == usr_info.email)
+
+            tickets = TicketsManager.filter(tickets.fetch(keys_only=True), list_search_terms)
 
             template_values = {
                 "info": AppInfo,
@@ -45,7 +75,8 @@ class TicketsManager(webapp2.RequestHandler):
                 "Priority": Ticket.Priority,
                 "Progress": Ticket.Progress,
                 "usr_info": usr_info,
-                "show_all": show_all
+                "show_all": show_all,
+                "search_terms": search_terms
             }
 
             jinja = jinja2.get_jinja2(app=self.app)
