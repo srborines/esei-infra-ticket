fc5808ede11c590c321443314a1821582ad476bc
samuel.borines <samuel.borines@amodosoluciones.com>
2018-05-19 02:06:12 +0200

    Modify tickets V2

--- handlers/tickets/modify.py
+++ handlers/tickets/modify.py
@@ -1,106 +1,89 @@
 #!/usr/bin/env python
-# coding: utf-8
 # (c) Baltasar 2018 MIT License <baltasarq@gmail.com>
 
 
 import webapp2
-import datetime as dt
-from google.appengine.api import users
 from google.appengine.ext import ndb
-from webapp2_extras import jinja2
 
-import model.user as usr_mgt
 import model.ticket as tickets
+from infra.globals import Globals
 from model.ticket import Ticket
-from model.appinfo import AppInfo
 
 
 class ModifyTicket(webapp2.RequestHandler):
-    def get(self):
-        try:
-            id = self.request.GET['ticket_id']
-        except KeyError:
-            self.redirect("/error?msg=ticket was not found")
-            return
+    def get(self, ticket_id):
+        # Get current user information
+        user, user_info = Globals.get_user_info()
 
-        usr = users.get_current_user()
-        usr_info = usr_mgt.retrieve(usr)
+        # Check if user is logged, if not redirect to home
+        if not user or not user_info:
+            webapp2.add_flash("not_logged_user")
+            return self.redirect("/")
 
-        if usr and usr_info:
-            access_link = users.create_logout_url("/")
+        # Get ticket by id
+        ticket = ndb.Key(urlsafe=ticket_id).get()
 
-            try:
-                ticket = ndb.Key(urlsafe=id).get()
-            except:
-                self.redirect("/error?msg=key #" + id + " does not exist")
-                return
+        # If ticket doesn't exist go to tickets index showing the error
+        if not ticket:
+            webapp2.add_flash("missing_ticket_title")
+            return self.redirect("/tickets")
 
-            template_values = {
-                "info": AppInfo,
-                "usr_info": usr_info,
-                "access_link": access_link,
-                "ticket": ticket,
-                "Status": Ticket.Status,
-                "Progress": Ticket.Progress,
-                "Priority": Ticket.Priority,
-                "Type": Ticket.Type,
-            }
+        # Prepare variables to send to view
+        template_variables = {
+            'ticket': ticket
+        }
 
-            jinja = jinja2.get_jinja2(app=self.app)
-            self.response.write(jinja.render_template("modify_ticket.html", **template_values))
-        else:
-            self.redirect("/")
+        # Render 'modify_ticket' view sending the variables 'template_variables'
+        return Globals.render_template(self, "modify_ticket.html", template_variables)
 
-    def post(self):
-        try:
-            id = self.request.GET['ticket_id']
-        except KeyError:
-            id = None
+    def post(self, ticket_id):
+        # Get current user information
+        user, user_info = Globals.get_user_info()
 
-        if not id:
-            self.redirect("/error?msg=missing id for modification")
-            return
+        # Check if user is logged, if not redirect to home
+        if not user or not user_info:
+            webapp2.add_flash("not_logged_user")
+            return self.redirect("/")
 
-        user = users.get_current_user()
-        usr_info = usr_mgt.retrieve(user)
+        # Get ticket by id
+        ticket = ndb.Key(urlsafe=ticket_id).get()
 
-        if user and usr_info:
-            # Get ticket by key
-            try:
-                ticket = ndb.Key(urlsafe=id).get()
-            except KeyError:
-                self.redirect("/error?msg=key " + id + " does not exist")
-                return
+        # If ticket doesn't exist go to tickets index showing the error
+        if not ticket:
+            webapp2.add_flash("missing_ticket_title")
+            return self.redirect("/tickets")
 
-            ticket.title = self.request.get("title", "").strip()
-            ticket.desc = self.request.get("desc", "").strip()
-            ticket.client_email = self.request.get("client_email", "").strip()
-            ticket.classroom = self.request.get("classroom", "").strip()
-            ticket.progress = Ticket.Progress.value_from_str(self.request.get("progress"))
-            ticket.status = Ticket.Status.value_from_str(self.request.get("status"))
-            ticket.priority = Ticket.Priority.value_from_str(self.request.get("priority"))
-            ticket.type = Ticket.Type.value_from_str(self.request.get("type"))
+        # Set all parameters from the request to ticket
+        ticket.title = self.request.get("title", "").strip()
+        ticket.desc = self.request.get("desc", "").strip()
+        ticket.client_email = self.request.get("client_email", "").strip()
+        ticket.classroom = self.request.get("classroom", "").strip()
+        ticket.progress = Ticket.Progress.value_from_str(self.request.get("progress"))
+        ticket.status = Ticket.Status.value_from_str(self.request.get("status"))
+        ticket.priority = Ticket.Priority.value_from_str(self.request.get("priority"))
+        ticket.type = Ticket.Type.value_from_str(self.request.get("type"))
 
-            # Chk
-            if len(ticket.title) < 1:
-                self.redirect("/error?msg=Aborted modification: missing title")
-                return
+        # If ticket title is missing return to modify view showing the error
+        if len(ticket.title) < 1:
+            webapp2.add_flash("missing_ticket_title")
+            return self.redirect("/tickets/modify/" + ticket_id)
 
-            if len(ticket.desc) < 1:
-                self.redirect("/error?msg=Aborted modification: missing desc")
-                return
+        # If ticket description is missing return to modify view showing the error
+        if len(ticket.desc) < 1:
+            webapp2.add_flash("missing_ticket_description")
+            return self.redirect("/tickets/modify/" + ticket_id)
 
-            # Report
-            tickets.send_email_for(ticket, "modified", "    modified by: " + str(usr_info))
+        # Send email report to broadcast email
+        # tickets.send_email_for(ticket, "modified", "    modified by: " + str(user_info))
 
-            # Save
-            tickets.update(ticket)
-            self.redirect("/info?url=/manage_tickets&msg=Ticket modified: "
-                          + ticket.title.encode("ascii", "replace"))
-        else:
-            self.redirect("/")
+        # Save ticket
+        tickets.update(ticket)
+
+        # Set successful message and redirect to tickets list
+        webapp2.add_flash("ticket_modified_successfully")
+        return self.redirect("/tickets")
 
 
 app = webapp2.WSGIApplication([
-    ("/tickets/modify", ModifyTicket),
+    ("/tickets/modify/(\d+)", ModifyTicket),
 ], debug=True)
