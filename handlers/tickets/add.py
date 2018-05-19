0a0ebd73bf29d38c032d46e84e76896be310fddf
samuel.borines <samuel.borines@amodosoluciones.com>
2018-05-19 02:07:32 +0200

    Add tickets V2

--- handlers/tickets/add.py
+++ handlers/tickets/add.py
@@ -2,25 +2,24 @@
 # (c) Baltasar 2018 MIT License <baltasarq@gmail.com>
 
 import webapp2
-from google.appengine.api import users
-
-
-import model.ticket as tickets
-import model.user as usr_mgt
+from infra.globals import Globals
 
 
 class AddTicket(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        usr_info = usr_mgt.retrieve(user)
+        # Get current user information
+        user, user_info = Globals.get_user_info()
 
-        if user and usr_info:
-            key = tickets.update(tickets.create(usr_info))
-            self.redirect("/tickets/modify?ticket_id=" + key.urlsafe())
-        else:
-            self.redirect("/")
+        # Check if user is logged, if not redirect to home
+        if not user or not user_info:
+            webapp2.add_flash("not_logged_user")
+            return self.redirect("/")
 
-        return
+        # Prepare variables to send to view
+        template_variables = {}
+
+        # Render 'add_ticket' view sending the variables 'template_variables'
+        return Globals.render_template(self, "add_ticket.html", template_variables)
 
 
 app = webapp2.WSGIApplication([
