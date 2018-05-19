d87a51f7076d5d02f9ec3132126bfde7c32725f7
samuel.borines <samuel.borines@amodosoluciones.com>
2018-05-19 02:05:31 +0200

    AppInfo moved to infra package

---
+++ infra/appinfo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# (c) Baltasar 2018 MIT License <baltasarq@gmail.com>
+
+
+class AppInfo:
+    Name = "esei :: infra :: ticket"
+    Version = "20180221-0200"
+    Author = "baltasarq@gmail.com"
+    AuthorWeb = "http://baltasarq.info/"
+    AppWeb = "http://esei-infra-ticket.appspot.com/"
+    AppEmail = "esei-infra-ticket@esei-infra-ticket.appspotmail.com"
+    # BroadcastEmail = "infraestructura@esei.uvigo.es"
+    BroadcastEmail = "srborines@esei.uvigo.es"
+    AllowedUserEmailNamespace = "@esei.uvigo.es"
